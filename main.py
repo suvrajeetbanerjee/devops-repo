fd855934e94cbffb3a15406e8ed7879b5f267a9e
Your Name <you@example.com>
2026-03-09 22:37:06 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello Build Triggers."}
+    model = {"title": "Hello DevOps Fans."}
     return render_template('index.html', model=model)
 
 if __name__ == "__main__":
